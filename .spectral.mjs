44e578c525619d275b6baedd5bcaefb9fb4955cb
qa-ilfts-0-qrzd@example.com <qa-ilfts-0-qrzd@example.com>
2023-05-10 17:15:29 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16434.stoplight-dev.com/api/v1/projects/cHJqOjExOTU/spectral.js?branch_slug=main&token=a763ac70-2700-4df5-9337-69fb1bbcac71";
+export default { extends: ruleset };
